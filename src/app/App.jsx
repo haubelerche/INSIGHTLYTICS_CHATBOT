7965b8d754bf41669cbab951605a84a6a54362de
haubelerche <HauLT.B22TC038@stu.ptit.edu.vn>
2025-11-09 12:14:52 +0700

    initial commit

---
+++ src/app/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AskForm from '../components/AskForm';
+
+export const App = () => {
+  return (
+    <div style={{minHeight:'100vh',background:'#051538ff',color:'#e6eef8',padding:24}}>
+      <AskForm />
+    </div>
+  );
+};
+
+export default App;
